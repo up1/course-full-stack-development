af1330a3374ef1aade7e6eb72d9f6f156d8c22f3
up1 <somkiat.p@gmail.com>
2024-04-27 17:19:41 +0700

    Refactor code

---
+++ workshop/demo02/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World from TypeScript!!!',
+  });
+});
+
+export default app;
--- workshop/demo02/src/server.ts
+++ workshop/demo02/src/server.ts
@@ -1,13 +1,6 @@
-import express from 'express';
+import app from './app';
 
-const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World from TypeScript!!!',
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);

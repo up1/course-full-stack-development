e6cfde86f129ed4bf89de178163af22a8b8c6d45
up1 <somkiat.p@gmail.com>
2024-04-28 14:14:16 +0700

    test to fail

---
+++ workshop/demo02/tests/post.api.failure.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import app from '../src/app';
+
+test('POST /posts :: Create a new post with error = 500', async () => {
+  const response = await request(app)
+    .post('/posts')
+    .send({
+      title: 'title',
+      // Simulate invalid data
+      content: undefined,
+    });
+  expect(response.status).toBe(500);
+  expect(response.body).toHaveProperty('error');
+});
+
+jest.mock('../src/client', () => ({
+  __esModule: true, // this property makes it work
+  default: {
+    $connect: jest.fn(),
+    post: {
+      findMany: jest.fn().mockImplementation(() => {
+        throw new Error('Database error');
+      }),
+    },
+  },
+}));
+test('GET /posts :: Get all posts with error = 500', async () => {
+  const response = await request(app).get('/posts');
+  expect(response.status).toBe(500);
+  expect(response.body).toHaveProperty('error');
+});

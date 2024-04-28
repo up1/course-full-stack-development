ca22b5fd3ebdcb7ac12a07b5a53f441d76e62752
up1 <somkiat.p@gmail.com>
2024-04-28 13:43:52 +0700

    Add test case

---
+++ workshop/demo02/tests/post.api.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+
+import app from '../src/app';
+
+test('GET /posts :: Success to get all posts', async () => {
+  const response = await request(app).get('/posts');
+  expect(response.status).toBe(200);
+  expect(Array.isArray(response.body)).toBe(true);
+});
+
+test('POST /posts :: Create a new post', async () => {
+  const response = await request(app)
+    .post('/posts')
+    .send({
+      title: 'title',
+      content: 'content',
+    });
+
+  expect(response.status).toBe(200);
+  expect(response.body).toHaveProperty('title', 'title');
+  expect(response.body).toHaveProperty('content', 'content');
+});
